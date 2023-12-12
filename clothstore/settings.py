cb0ea17e5811e9f9c308f7308251fe52e9062a83
fcgsam <fcgaavez@gmail.com>
2023-12-12 12:38:30 +0530

    handle error in tempates dict

--- clothstore/settings.py
+++ clothstore/settings.py
@@ -64,7 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'ClothApp.context_proccer.menu_link_'
+                'ClothApp.context_proccer.menu_link'
             ],
         },
     },
