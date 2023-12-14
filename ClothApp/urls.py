e5c62fc9e30d664a189ca0790b7e631070c13d0a
new_name <new_name@gmail.com>
2023-12-14 09:05:32 +0530

    14-12-2023

--- ClothApp/urls.py
+++ ClothApp/urls.py
@@ -3,4 +3,5 @@ from . import views              #from views import home_page
 
 urlpatterns = [
     path('',views.home_page,name='home'),
+    path('product-detail/<int:id>/',views.productDetails,name='product-detail'),
 ]
--- ClothApp/views.py
+++ ClothApp/views.py
@@ -8,3 +8,10 @@ def home_page(request):
         'info': all_data
     }
     return render(request,'index.html',context)
+
+def productDetails(request,id):
+    get_data = ProductModel.objects.get(id=id)
+    context ={
+        'get_info' : get_data
+    }
+    return render(request,'product_detail.html',context)
