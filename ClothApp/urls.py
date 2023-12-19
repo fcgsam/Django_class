370bc1c4be9a2d29ed7cff3b4b4f55c8969d0963
new_name <new_name@gmail.com>
2023-12-19 09:07:04 +0530

    19-12-2023

--- ClothApp/urls.py
+++ ClothApp/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('',views.home_page,name='home'),
     path('product-detail/<int:id>/',views.productDetails,name='product-detail'),
     path('category_name/<int:id>',views.category_name,name='category_name'),
+    path('search/',views.search_product,name='search'),
 ]
--- ClothApp/views.py
+++ ClothApp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,get_object_or_404
 from .models import ProductModel,Category
-from django.http import HttpResponse
+from django.http import HttpResponse,Http404
 # Create your views here.
 def home_page(request):
     all_data = ProductModel.objects.all()
@@ -8,6 +8,7 @@ def home_page(request):
         'info': all_data
     }
     return render(request,'index.html',context)
+
 
 def productDetails(request,id):
     get_data = ProductModel.objects.get(id=id)
@@ -25,3 +26,18 @@ def category_name(request,id):
         'info' : product
     }
     return render(request,'product-category-detail.html',context)
+
+def search_product(request):
+    if request.method == 'GET':
+        search_data = request.GET.get('search')
+        if(len(search_data)==0):
+            raise Http404('please provide valid data')
+        else:
+            # for description too first import from django.db.models import Q
+            # result = ProductModel.objects.filter(Q(product_name__icontains = search_data) | Q(product_description__icontains = search_data))
+            result = ProductModel.objects.filter(product_name__icontains = search_data)
+            context = {
+                'info':result
+            }
+
+            return render(request,'search.html',context)
